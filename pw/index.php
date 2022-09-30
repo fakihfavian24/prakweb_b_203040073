c4dfb618d3998935c4986ffefcb90a310e40914b
fakihfavian24 <fakihfavianwibowo@gmail.com>
2022-09-30 15:31:20 +0700

    Mengubah Warna  Background

--- pw/index.php
+++ pw/index.php
@@ -15,7 +15,7 @@ $buku  = query("SELECT * FROM buku");
   <!-- link font awesome cdn -->
   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css">
 
-  <link rel="stylesheet" href="style1.css">
+  <link rel="stylesheet" href="style2.css">
 
   <title>Halaman Admin</title>
 </head>
