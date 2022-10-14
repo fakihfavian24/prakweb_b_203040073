6c7415e0cf7c3fe4c6ed04713eb5e0cb10c9466d
fakihfavian24 <fakihfavianwibowo@gmail.com>
2022-10-14 13:33:48 +0700

    Membuat MVC #Database Wrapper

--- mvc/app/core/Constants.php
+++
@@ -1,3 +0,0 @@
-<?php
-
-define('BASEURL', 'http://localhost/prakweb_b_203040073/mvc/public');
--- mvc/app/models/Mahasiswa_model.php
+++ mvc/app/models/Mahasiswa_model.php
@@ -2,47 +2,24 @@
 
 class Mahasiswa_model
 {
-  // private $mhs = [
-  //   [
-  //     "nama" => "Fakih Favian",
-  //     "nrp" => "203040073",
-  //     "email" => "203040073@mail.unpas.ac.id",
-  //     "jurusan" => "Teknik Informatika"
-  //   ],
-  //   [
-  //     "nama" => "Fajar",
-  //     "nrp" => "203040074",
-  //     "email" => "203040020@mail.unpas.ac.id",
-  //     "jurusan" => "Teknik mesin"
-  //   ],
-  //   [
-  //     "nama" => "Alfi",
-  //     "nrp" => "203040054",
-  //     "email" => "203040054@mail.unpas.ac.id",
-  //     "jurusan" => "Teknik Industri"
-  //   ]
-  // ];
-
-  // database handler
-  private $dbh;
-  private $stmt;
+  private $table = 'mahasiswa';
+  private $db;
 
   public function __construct()
   {
-    // data source name
-    $dsn = 'mysql:host=localhost;dbname=phpmvc';
-
-    try {
-      $this->dbh = new PDO($dsn, 'root', '');
-    } catch (PDOException $e) {
-      die($e->getMessage());
-    }
+    $this->db = new Database;
   }
 
   public function getAllMahasiswa()
   {
-    $this->stmt = $this->dbh->prepare('SELECT *FROM mahasiswa');
-    $this->stmt->execute();
-    return $this->stmt->fetchAll(PDO::FETCH_ASSOC);
+    $this->db->query('SELECT * FROM ' . $this->table);
+    return $this->db->resultSet();
+  }
+
+  public function getMahasiswaById($id)
+  {
+    $this->db->query('SELECT * FROM ' . $this->table . ' WHERE id=:id');
+    $this->db->bind('id', $id);
+    return $this->db->single();
   }
 }
